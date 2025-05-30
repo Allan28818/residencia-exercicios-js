9ec225a3fd310a929495080f0471b5d538b021d6
Lucas Gabriel <lucasgcsousa2005@gmail.com>
2025-05-30 17:53:41 -0300

    Grupo 2

--- aula-nextjs/src/app/page.tsx
+++ aula-nextjs/src/app/page.tsx
@@ -1,6 +1,19 @@
-"use client";
-import styles from "./page.module.css";
+'use client';
+import { useState } from 'react';
+import styles from "../page.module.css"; // ajuste o caminho se necessário
 
-export default function Home() {
-  return <button className={styles.page}></button>;
+export default function TemaI() {
+  const [contador, setContador] = useState(1);
+
+  function clicar() {
+    setContador(1);
+  }
+
+  return (
+    <div className={styles.page}>
+      <p>Contador: {contador}</p>
+      <button onClick={clicar}>Resetar contador para 1</button>
+      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+    </div>
+  );
 }
