c9018cd0ce28104ef923c977582601bbd187c95b
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2025-06-02 15:00:40 -0300

    Update and rename page.tsx to tema-II.tsx

--- aula-nextjs/src/app/tema-II.tsx
+++ aula-nextjs/src/app/tema-II.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function TemaII() {
   
   const [texto, setTexto] = useState("");
 
